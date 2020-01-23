a712708e71c19fcf2fa211a25b1c1fb464682160
Mauricio503 <mauriciogspagnol@gmail.com>
2020-01-23 20:36:02 +0000

    criado gitignore

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ const EstrategiaController = require("./controller/EstrategiaController");
 
 const routes = Router();
 
-routes.get("/estrategia/odd120a150", EstrategiaController.odd120a150);
+routes.get("/estrategia/teste", EstrategiaController.teste);
 routes.post("/estrategia/pesquisa", EstrategiaController.pesquisa);
 
 module.exports = routes;
