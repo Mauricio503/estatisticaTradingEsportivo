65b8062adacde2c7bb7603f6907c20cf212c5d8e
Mauricio503 <mauriciogspagnol@gmail.com>
2020-01-24 00:12:10 +0000

    corrigindo erro de upload

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const{ Router } = require('express');
-const EstrategiaController = require("./controller/EstrategiaController");
-
-const routes = Router();
-
-routes.get("/estrategia/teste", EstrategiaController.teste);
-routes.post("/estrategia/pesquisa", EstrategiaController.pesquisa);
-
-module.exports = routes;
