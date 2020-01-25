adc7be7a861511000e61709cb6f931f3bb12b2f0
Mauricio503 <mauriciogspagnol@gmail.com>
2020-01-25 11:50:53 +0000

    arrumando dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
-require('dotenv').load();
+const dotenv = require('dotenv');
 
 const app = express();
 
